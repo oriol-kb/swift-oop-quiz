e6ab7db620e09a1b82f242c1d92be08435e4e6fd
oriol <oriol.castello@reclamadatos.es>
2019-03-05 20:01:28 +0100

    Initial Commit

---
+++ swquizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swquizz
+//
+//  Created by Oriol Castelló Bregolat on 5/3/19.
+//  Copyright © 2019 Mian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
